be0668b17801fae168cf66c26f868604d06222e0
Ig-0r <igorlfilgueiras@gmail.com>
2025-05-16 15:49:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagemDiv = document.getElementById('mensagem');
+    mensagemDiv.textContent = 'Você é capaz de alcançar qualquer objetivo! Continue focado!';
+}
